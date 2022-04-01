e92cf922fddc41cc528090f70872b8bba28882d6
Edward Gunn <edsgunn@outlook.com>
2022-04-01 17:59:55 +0100

    Add dropout

--- Model/Generator.py
+++ Model/Generator.py
@@ -11,7 +11,8 @@ class LSTM_Generator_Model(nn.Module):
         self.device = device
         self.fc_layer_1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
-        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True, bidirectional=False)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True, dropout=0.2, bidirectional=False)
+        # self.dropout = nn.Dropout(0.2) 
         self.fc_layer_2 = nn.Linear(hidden_size, output_size)
         self.softmax = nn.Softmax(dim=2)
         pass
@@ -25,6 +26,7 @@ class LSTM_Generator_Model(nn.Module):
         out = self.fc_layer_1(x)
         out = self.relu(out)
         out, hidden = self.lstm(out, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
+        # out = self.dropout(out) 
         out = self.fc_layer_2(out)
         out = self.softmax(out)
 
